482fd97e063b6bf93c29adaf9c3e69bafd7bd73d
autyh <ampur015@gmail.com>
2021-03-30 13:45:00 +0900

    commit

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+import socket
 
 base_dir = os.path.dirname(os.path.realpath(__file__))
 
@@ -6,11 +7,13 @@ class BaseConfig(object):
     ENV = None
     Testing = False
 
-
 class DevelopmentConfig(BaseConfig):
+    host = socket.gethostname()
+    localhost = socket.gethostbyname(host)
+
     ENV = 'development'
     # runsever Setting
-    SERVER_IP='locahost'
+    SERVER_IP=localhost
     SERVER_PORT=8000
     # Session Setting
     SECRET_KEY = 'your secret key'
